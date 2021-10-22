161ca043bae6567ad07b346851affdf9362b162c
ambarnett <andy.barnett09@gmail.com>
2021-10-21 22:35:53 -0500

    inital commit

---
+++ LineMate/client/src/components/ApplicationViews.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Home from "./homePage/Home";
+
+export default function ApplicationViews({isLoggedIn }){
+    return (
+        <main>
+            <Switch>
+                <Route path="/" exact>
+                    {isLoggedIn ? <Home /> : <Redirect to="/login" />}
+                </Route>
+
+                <Route path="/login">
+                    <Login />
+                </Route>
+
+                <Route path="/register">
+                    <Register />
+                </Route>
+            </Switch>
+        </main>
+    )
+}
---
+++ LineMate/client/src/components/homePage/Home.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function Hello() {
+    return (
+        <span
+            style={{
+                position: "fixed",
+                left: 0,
+                right: 0,
+                top: "50%",
+                marginTop: "-0.5rem",
+                textAlign: "center",
+            }}
+        >
+            <h1>
+                Welcome to Tabloid
+                <br />
+                <i>The Facebook of Blog Posts</i>
+            </h1>
+        </span>
+    )
+}
